f5f3f2534a7c3bb17fd8d814df15e41e823108d9
d-meizin <d.meizin@gmail.com>
2026-01-04 01:10:22 +0300

    Initialize commit

---
+++ src/main/java/com/employee/FullstackFirstTableBackApplication.java
@@ -0,0 +1,13 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackFirstTableBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackFirstTableBackApplication.class, args);
+	}
+
+}
